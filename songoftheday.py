1da23398f5f2663206c8fd8707718128347800d1
Ben Sampson <bensampson5@gmail.com>
2020-07-24 10:34:00 -0500

    Updated .gitignore, added sox mod of wav, and added font

--- songoftheday.py
+++ songoftheday.py
@@ -5,6 +5,7 @@ import datetime
 import requests
 import httplib2
 import subprocess
+from pathlib import Path
 import xml.etree.ElementTree as et
 from urllib.parse import urlencode
 from oauth2client import client
@@ -13,12 +14,6 @@ from oauth2client.file import Storage
 from googleapiclient import discovery
 from PIL import Image, ImageDraw, ImageFont
 
-# If modifying these scopes, delete your previously saved credentials
-# at ~/.credentials/calendar-python-songoftheday.json
-SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
-CLIENT_SECRET_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'client_secrets.json')
-#print(CLIENT_SECRET_FILE)
-APPLICATION_NAME = 'Song of the Day'
 
 def get_credentials():
     """Gets valid user credentials from storage.
@@ -29,20 +24,25 @@ def get_credentials():
     Returns:
         Credentials, the obtained credential.
     """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'calendar-python-songoftheday.json')
+    cwd = Path('.')
+    credentials_dir = cwd / '.credentials'
+    if not credentials_dir.exists():
+        credentials_dir.mkdir()
+    credential_path = credentials_dir / 'calendar-python-songoftheday.json'
 
     store = Storage(credential_path)
     credentials = store.get()
     if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
+
+        # try to find a client_secrets json file in the current directory
+        client_secret = list(cwd.glob('client_secret*.json'))[0]
+
+        # validate credentials using client_secret
+        scopes = 'https://www.googleapis.com/auth/calendar.readonly'
+        flow = client.flow_from_clientsecrets(client_secret, scopes)
+        flow.user_agent = 'Song of the Day'
         credentials = tools.run_flow(flow, store)
-        print('Storing credentials to ' + credential_path)
+        print('Storing credentials to ' + str(credential_path))
     return credentials
 
 
@@ -94,7 +94,8 @@ def get_itunes_song_data(search_terms="", random=False):
 
     if not random:
         # url encode search term input from user. Only look for songs
-        query = urlencode({'term': search_terms, 'media': 'music', 'entity': 'song', 'explicit': 'No'})
+        query = urlencode(
+            {'term': search_terms, 'media': 'music', 'entity': 'song', 'explicit': 'No'})
 
         # request data from iTunes and process returned json object
         r = requests.get("https://itunes.apple.com/search?" + query)
@@ -110,14 +111,16 @@ def get_itunes_song_data(search_terms="", random=False):
         # that has a valid 30 second preview on iTunes
 
         # pull all items from Song of the Day feed from The Current
-        r = requests.get("https://feeds.publicradio.org/public_feeds/song-of-the-day/rss/rss")
+        r = requests.get(
+            "https://feeds.publicradio.org/public_feeds/song-of-the-day/rss/rss")
         rss_root = et.fromstring(r.content.decode("utf-8"))
         items_sotd = rss_root.find('channel').findall('item')
 
         # iterate through each item on Song of the Day feed from The Current and look up on iTunes
         # for a match
         for i in items_sotd:
-            query = urlencode({'term': i.find('title').text, 'media': 'music', 'entity': 'song'})
+            query = urlencode({'term': i.find('title').text,
+                               'media': 'music', 'entity': 'song'})
 
             # request data from iTunes and process returned json object
             r = requests.get("https://itunes.apple.com/search?" + query)
@@ -128,10 +131,15 @@ def get_itunes_song_data(search_terms="", random=False):
                 if result['previewUrl']:
                     return result
 
+
 if __name__ == "__main__":
 
-    FILE_BASE_NAME = 'songoftheday'  # base name for audio files
-    SONGS_DIR = os.path.join(os.path.split(os.path.dirname(os.path.abspath(__file__)))[0], "songs")
+    song_file_base_name = 'songoftheday'  # base name for audio files
+    cwd = Path('.')
+    data_dir = cwd / 'data'
+
+    if not data_dir.exists():
+        data_dir.mkdir()
 
     sotd_events = get_eligible_sotd_events()
 
@@ -159,76 +167,101 @@ if __name__ == "__main__":
     if sotd_itunes_data:
 
         # delete any old song of the day audio files if they exist
-        files = os.listdir(SONGS_DIR)
-        for f in files:
-            if f.endswith('.wav') or f.endswith('.m4a'):
-                os.remove(os.path.join(SONGS_DIR, f))
+        for p in data_dir.glob('*.wav'):
+            p.unlink()
+        for p in data_dir.glob('*.m4a'):
+            p.unlink()
 
         # grab 30 second song preview (is in .m4a format)
         preview_m4a = requests.get(sotd_itunes_data['previewUrl'])
 
         # save data bytes to .m4a file
-        m4a_fname = os.path.join(SONGS_DIR, FILE_BASE_NAME + str(datetime.date.today()) + '.m4a')
+        m4a_fname = data_dir / (song_file_base_name +
+                                '_' + str(datetime.date.today()) + '.m4a')
         with open(m4a_fname, 'wb') as f:
             f.write(preview_m4a.content)
 
         # use ffmpeg to open the .m4a file and convert to .wav file
-        wav_fname = os.path.join(SONGS_DIR, FILE_BASE_NAME + str(datetime.date.today()) + '.wav')
-        process = subprocess.Popen(['ffmpeg', '-i', m4a_fname, wav_fname], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate()
-        # print(stdout, stderr)
+        wav_fname = data_dir / (song_file_base_name +
+                                '_' + str(datetime.date.today()) + '.wav')
+        cmd = ' '.join(['ffmpeg', '-i', str(m4a_fname), str(wav_fname)])
+        proc = subprocess.run(cmd, capture_output=True, timeout=10, shell=True)
+        if proc.returncode != 0:  # check if completed successfully
+            if proc.stdout:
+                print('stdout:\n' + proc.stdout.decode() + '\n')
+            if proc.stderr:
+                print('stderr:\n' + proc.stderr.decode() + '\n')
+            raise Exception('m4a to wav conversion using ffmpeg failed')
 
-        # Log completed request in songs.log
+        # use sox to modify wav file for speaker system
+        cmd = ' '.join(['sox', str(wav_fname), '-c 1', str(wav_fname).split('.')[0] + '_modified' +
+                        wav_fname.suffix, 'lowpass 5000 silence 1 3 2% compand 0.3,1 6:-70,-60,-20 -5 -90 0.2'])
+        proc = subprocess.run(cmd, capture_output=True, timeout=10, shell=True)
+        if proc.returncode != 0:  # check if completed successfully
+            if proc.stdout:
+                print('stdout:\n' + proc.stdout.decode() + '\n')
+            if proc.stderr:
+                print('stderr:\n' + proc.stderr.decode() + '\n')
+            raise Exception('wav modification using sox failed')
+
+        # Grab important information from song of the day
         dt = str(datetime.datetime.now())
         track = sotd_itunes_data['trackName']
         artist = sotd_itunes_data['artistName']
         album = sotd_itunes_data['collectionName']
-        log_string = "Timestamp: " + dt + "\n" + "Requester: " + requester + "\n"\
-                    + "Track: " + track + "\n" + "Artist: " + artist + "\n"\
-                    + "Album: " + album + "\n\n"
-        album_art_Url = sotd_itunes_data['artworkUrl100']
-    #    print(album_art_Url)
-        album_art_Url = album_art_Url.replace("100x100","400x400")
-    #    print(album_art_Url)
 
-        album_bitmap = requests.get(album_art_Url)    #  raw jpg bitmap
-    #    print(album_bitmap)
+        album_art_Url = sotd_itunes_data['artworkUrl100'].replace(
+            "100x100", "400x400")
+        album_bitmap = requests.get(album_art_Url)  # raw jpg bitmap
 
-        bitmap_fname = os.path.join(SONGS_DIR,"bitmap.jpg")
+        bitmap_fname = data_dir / 'bitmap.jpg'
         if album_bitmap.status_code == 200:
             with open(bitmap_fname, 'wb') as f:
                 f.write(album_bitmap.content)
         else:
-            os.remove(bitmap_fname)
-        
+            bitmap_fname.unlink()
+
         album_bitmap = Image.open(bitmap_fname)
-        bkgnd = Image.new("RGB",(1024,768),"black")
-        (sizeX,sizeY) = album_bitmap.size
-        bkgnd.paste(album_bitmap,(512-sizeX//2,768-sizeY))  # note integer division
-        draw = ImageDraw.Draw(bkgnd)                      #  create pointer to draw on bkgnd
-        font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf",40)  # generate font
+        bkgnd = Image.new("RGB", (1024, 768), "black")
+        (sizeX, sizeY) = album_bitmap.size
+        # note integer division
+        bkgnd.paste(album_bitmap, (512-sizeX//2, 768-sizeY))
+        draw = ImageDraw.Draw(bkgnd)  # create pointer to draw on bkgnd
+
+        fonts_dir = cwd / 'fonts'
+        fonts_path = fonts_dir / 'DejaVuSansMono.ttf'
+
+        font = ImageFont.truetype(str(fonts_path), 40)  # generate font
         title = "Song of the Day"
-        (width,height) = draw.textsize(title,font=font)
-        draw.text(((1024-width)/2,20),title,fill="white",font=font)
+        (width, height) = draw.textsize(title, font=font)
+        draw.text(((1024-width)/2, 20), title, fill="white", font=font)
 
-        draw.text((40,80), "Song: "+track ,fill="white",font=font)
-        draw.text((40,140), "Artist: "+artist ,fill="white",font=font)
-        draw.text((40,200), "Album: "+album ,fill="white",font=font)
+        draw.text((40, 80), "Song: "+track, fill="white", font=font)
+        draw.text((40, 140), "Artist: "+artist, fill="white", font=font)
+        draw.text((40, 200), "Album: "+album, fill="white", font=font)
 
-        font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf",30)  # generate font
-        draw.text((40,260), "Email Complaints To: "+requester,fill="white",font=font)
+        fonts_dir = cwd / 'fonts'
+        font = ImageFont.truetype(str(fonts_path), 30)  # generate font
+        draw.text((40, 260), "Email Complaints To: " +
+                  requester, fill="white", font=font)
 
-        bkgnd.save("~/Pictures/sotd.png")
+        bkgnd.save(data_dir / 'sotd.png')
 
-
-        # log completion of script
-        with open(os.path.join(SONGS_DIR, "songs.log"), 'a') as log:
-            log.write(log_string)
+        # log new songoftheday entry
+        log_dir = cwd / 'logs'
+        log_fname = 'songs.log'
+        if not log_dir.exists():
+            log_dir.mkdir()
+        log_entry = "Timestamp: " + dt + "\n" + "Requester: " + requester + "\n" \
+            + "Track: " + track + "\n" + "Artist: " + artist + "\n" \
+            + "Album: " + album + "\n\n"
+        with open(log_dir / log_fname, 'a') as log:
+            log.write(log_entry)
 
     else:
         # Log error
         dt = str(datetime.datetime.now())
-        log_string = "Timestamp: " + dt + "\n" + "Error: Failed to find a song for Song of the Day\n\n"
-        with open(os.path.join(SONGS_DIR, "songs.log"), 'a') as log:
-            log.write(log_string)
-
+        log_string = "Timestamp: " + dt + "\n" + \
+            "Error: Failed to find a song for Song of the Day\n\n"
+        with open(log_dir / log_fname, 'a') as log:
+            log.write(log_entry)
